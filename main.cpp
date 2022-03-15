dfca209d39f20f51cc9029821dadb806d9982073
Amr Mekki <amr.mekki21@gmail.com>
2022-03-15 22:08:21 +0200

    Initialized OpenGL Screen with all its requirements

--- main.cpp
+++ main.cpp
@@ -6,9 +6,43 @@
 
 using namespace std;
 
+void displayCallback();
+void render();
+void changeViewPort(int, int);
+
 int main(int argc, char **argv) {
 	
 	glutInit(&argc, argv);
-	glutInitDisplayMode();
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	//glutInitWindowPosition(200, 100);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("SNAKE");
+	glutDisplayFunc(displayCallback);
+	glutReshapeFunc(changeViewPort);
+	render();
+	glutMainLoop();
+
+
+
+
+	
 	return 0;
+}
+
+void changeViewPort(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0,40,0,40,-1,1);
+}
+
+void render() {
+
+	glClearColor(1,0,0,1);
+}
+
+void displayCallback() {
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
 }
