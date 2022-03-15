90f968aab2d69c58523cd03e17e1cb20e7373d6a
Amr Mekki <amr.mekki21@gmail.com>
2022-03-15 21:40:28 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//Jeff Chastine
+#include <Windows.h>
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode();
+	return 0;
+}
